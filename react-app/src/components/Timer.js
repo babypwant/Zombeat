7fb8f283292dcdd71b59831e5aeab182f149a004
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-08-02 13:54:57 -0700

    Starting timer CRUD with store

--- react-app/src/components/Timer.js
+++ react-app/src/components/Timer.js
@@ -5,13 +5,18 @@ import MusicBar from './MusicBar';
 import './styles/Playlist.css'
 
 const Timer = () => {
+    const [timerName, setTimerName] = useState('')
     const history = useHistory();
-
     const user = useSelector(state => state.session.user);
     const user_id = user.id
+
     useEffect(() => {
     }, []);
 
+    const createTimer = (e) => {
+        e.preventDefault();
+        
+    }
 
 
     return (
@@ -19,6 +24,13 @@ const Timer = () => {
             <div className='playlist-main-content'>
                 <form className='album-form' method="POST" action="/playlists/">
                     <label>New timer name</label>
+                    <input className='timer-name-input'
+                        onChange={(e) => setTimerName(e.target.value)}
+                    >
+
+                    </input>
+                    <button onClick={createTimer}>Create your timer</button>
+                    <button>Delete your timer</button>
                 </form>
             </div>
             <div className='content-container'>
