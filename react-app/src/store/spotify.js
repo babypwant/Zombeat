ea2850056ac13665d009b80c4b762a22c02a5e87
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-08-03 15:34:49 -0700

    setup, for env acccess token

--- react-app/src/store/spotify.js
+++ react-app/src/store/spotify.js
@@ -1,1 +1,18 @@
 const SET_ACCESS_TOKEN = "SET_ACCESS_TOKEN"
+
+
+const setAccessToken = (accessToken) => ({
+    type: SET_ACCESS_TOKEN,
+    accessToken
+})
+
+export const getAccessToken = () => {
+    const response = await fetch(`https://accounts.spotify.com/api/token`, {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' + btoa(`` + `:` + ``),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials'
+    })
+}
