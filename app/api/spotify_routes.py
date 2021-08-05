c1aaf26f678459661f5e2504abc67ac252ef3565
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-08-05 12:51:43 -0700

    keys not working

--- app/api/spotify_routes.py
+++ app/api/spotify_routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 import base64
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+import urllib
+import requests
 import os
 from flask.wrappers import Response
 
@@ -13,19 +15,23 @@ spotify_routes = Blueprint('spotify', __name__)
 def get_token():
     client_id = os.environ['CLIENT_ID']
     client_secret = os.environ['CLIENT_SECRET']
-    ascii_id = client_id.encode('ascii')
-    ascii_secret = client_secret.encode('ascii')
-    encode_id = base64.b64encode(ascii_id)
-    encode_secret = base64.b64encode(ascii_secret)
-    headers = {
-        'Authorization': 'Basic ' + f'${encode_id}' + ':' + f'${encode_secret}',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    body = {'grant_type=client_credentials'}
-    post_response = Request(
-        'https://accounts.spotify.com/api/token', headers, body)
-    if post_response.status_code == 200:
-        pr = post_response.json()
-        return pr['access_token'], pr['refresh_token'], pr['expires_in']
-    else:
-        return ("Error ===============================================================", post_response.status_code)
+    print("HERE IS CLIENT ID", client_id)
+    print("HERE IS CLIENT ID", client_secret)
+
+    post_response = requests.post(
+        'https://accounts.spotify.com/api/token',
+        data='grant_type=client_credentials',
+        headers={
+            'Authorization': 'Basic ' + f'${client_id}' + ':' + f'${client_secret}',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        })
+    tok = post_response.json()
+    print("HERE IS YOUR RESPONSE", tok)
+
+    return tok
+    # if post_response:
+    #     pr = post_response.json()
+    #     print("DATA HERE", pr)
+    #     return{"NICE, BAXKEND HERE": "Cool"}
+    # else:
+    #     return ("Error ===============================================================", post_response.status_code)
