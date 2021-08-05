c34565ba6d8b91e3e7064c20ea6651897ace478c
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-08-05 13:57:16 -0700

    Normalizing front end

--- react-app/src/components/Playlist.js
+++ react-app/src/components/Playlist.js
@@ -1,9 +1,15 @@
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getPlaylists } from '../store/playlists';
+import { getAllTimers } from '../store/timer';
+import playlistIcon from '../components/styles/images/playlist-icon.jpg'
+import timerIcon from '../components/styles/images/add-timer.png'
+
 import Timer from './Timer';
 import './styles/Playlist.css'
 import MusicBar from './MusicBar';
+
 
 const Playlist = () => {
     const history = useHistory();
@@ -11,8 +17,10 @@ const Playlist = () => {
     const [new_name, setNewName] = useState('')
     const [playlistName, setPlaylistName] = useState('')
     const user = useSelector(state => state.session.user);
+    const allPlaylists = useSelector(state => state.playlists)
+    const allTimers = useSelector(state => state.timers?.undefined?.all_timers)
     const user_id = user.id
-
+    const dispatch = useDispatch();
 
     useEffect(() => {
         (async () => {
@@ -26,6 +34,8 @@ const Playlist = () => {
             });
             const responseData = await response.json();
             setPlaylistId(responseData?.id)
+            dispatch(getPlaylists(user.id))
+            dispatch(getAllTimers(user.id))
         })()
     }, []);
 
@@ -77,6 +87,23 @@ const Playlist = () => {
         history.push('/dashboard')
     }
 
+    const createTimer = (e) => {
+        e.preventDefault();
+        history.push('/new/timer')
+    };
+
+    const editTimer = (e) => {
+        e.preventDefault();
+        history.push(`/edit/timer/${e.target.value}`)
+        console.log(1)
+    };
+
+    const editPlaylist = (e) => {
+        e.preventDefault();
+        history.push(`/edit/playlist/${e.target.value}`)
+    };
+
+
     return (
         <div className='dashboard-main-container'>
             <div className='playlist-main-content'>
@@ -96,8 +123,47 @@ const Playlist = () => {
                 </form>
             </div>
             <div className='content-container'>
-                <div className='here' onClick={makeNewPlaylist}>
-                    Create Playlist
+                <div className='create-playlist-btn' onClick={makeNewPlaylist}>
+                    <img className='new-playlist-icon' src={playlistIcon} />
+                    <label className='create-playlist-label'> Create Playlist </label>
+                </div>
+                <div className='timers-container' onClick={createTimer}>
+                    <img className='new-timer-icon' src={timerIcon} />
+                    <label className='timer-label'>Create a Timer</label>
+                </div>
+                <div className='timers'>
+                    <ul>
+
+                        {allTimers &&
+                            allTimers.map((timer) => {
+                                return (
+                                    <li value={timer.id}
+                                        onClick={editTimer}
+                                        className='timer-li'
+                                        key={timer.id}
+                                    >{timer.name}</li>
+                                )
+                            })
+
+                        }
+                    </ul>
+                </div>
+                <div className='all-playlists-container'>
+                    <ul>
+                        {allPlaylists &&
+                            Object.values(allPlaylists).map((playlist) => {
+                                return (
+                                    <li key={playlist.id}
+                                        className={`playlist-btn`}
+                                        value={playlist.id}
+                                        onClick={editPlaylist}
+                                    >
+                                        {playlist.name}
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
                 </div>
             </div>
             <MusicBar />
