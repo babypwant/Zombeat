71ff255bc20a8adc04c2378a42ae976422320aab
Gary Rios <78942879+babypwant@users.noreply.github.com>
2021-08-08 23:23:53 -0700

    Done w code for tonight

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,6 +1,10 @@
 import clouds from '../components/styles/images/clouds.png'
 import './styles/Home.css'
 import { Dispatch } from 'react';
+import { Redirect } from 'react-router';
+import gitCloud from '../components/styles/images/cloud-git.png'
+import linkedCloud from '../components/styles/images/linked.png'
+
 import { useDispatch } from 'react-redux';
 import { login } from '../store/session';
 import { useHistory } from 'react-router';
@@ -15,6 +19,14 @@ const Home = () => {
         history.push('/dashboard')
     }
 
+    const gitHub = (e) => {
+        e.preventDefault();
+        window.location.href = "https://github.com/babypwant"
+    }
+    const linkedIn = (e) => {
+        e.preventDefault();
+        window.location.href = "https://www.linkedin.com/in/gary-rios-9464a2208"
+    }
     return (
         <div className='main-container'>
             <div className='background'>
@@ -29,10 +41,16 @@ const Home = () => {
                 </div>
                 <div className='clouds-container'>
                     <div>
-                        <div onClick={demoLogin}>
-                            <img src={clouds} alt="pixel-clouds"></img>
+                        <div >
+                            <img className='pixel-clouds' src={clouds} alt="pixel-clouds" onClick={demoLogin}></img>
                         </div>
                     </div>
+                    <div className='git-cloud-container'>
+                        <img className='git-cloud-img' onClick={gitHub} src={gitCloud} />
+                    </div>
+                </div>
+                <div>
+                    <img className='linked-cloud' onClick={linkedIn} src={linkedCloud} />
                 </div>
             </div>
         </div >
